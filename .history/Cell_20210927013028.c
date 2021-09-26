2c95b2cb2751e5abe0b6ddda0f8dcde064c533a3
owya490 <owenyang2002@outlook.com>
2021-09-27 01:50:05 +1000

    cell update
    cell updated

--- .history/Cell_20210927013028.c
+++ .history/Cell_20210927013028.c
@@ -4,28 +4,31 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+struct tmp{
+    int value;
+};
 
 struct cell {
     bool fill;
     char display;
+    struct tmp grid[5][5];
 };
 
 
 struct cell* cell_constructor() {
     struct cell* c = malloc(sizeof(struct cell));
     c->fill = false;
-    c->display = ' ';
+    c->display = 'o';
     return c;
 }
 
 void cell_flip(struct cell* c) {
     if (c->fill == true) {
         c->fill = false;
-        c->display = ' ';
+        c->display = 'o';
     }
     else {
         c->fill = true;
         c->display = 'x';
     }
 }
-
